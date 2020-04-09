992bb0f4a9ab1185b42a3e63e0c5aa7bb77c0979
codimiracle <codimiracle@outlook.com>
2020-04-10 04:40:35 +0800

    enhanced notification template and fix null pointer for book data

--- src/main/java/com/codimiracle/application/platform/huidu/helper/HuiduNotificationTemplate.java
+++ src/main/java/com/codimiracle/application/platform/huidu/helper/HuiduNotificationTemplate.java
@@ -1,6 +1,5 @@
 package com.codimiracle.application.platform.huidu.helper;
 
-import com.codimiracle.application.platform.huidu.entity.po.Book;
 import com.codimiracle.application.platform.huidu.entity.po.Notification;
 import com.codimiracle.application.platform.huidu.entity.po.Subscribe;
 import com.codimiracle.application.platform.huidu.entity.vo.BookVO;
@@ -19,7 +18,7 @@ public class HuiduNotificationTemplate implements NotificationTemplate {
     private BookService bookService;
 
     @Override
-    public Notification generateBy(SubscribeType type, String receiverId, String targetId) {
+    public Notification generateSubscribeBy(SubscribeType type, String receiverId, String targetId) {
         if (type == SubscribeType.BookUpdated) {
             BookVO bookVO = bookService.findByIdIntegrally(targetId);
             Notification notification = new Notification();
@@ -33,14 +32,34 @@ public class HuiduNotificationTemplate implements NotificationTemplate {
     }
 
     @Override
+    public Notification generateNotificationBy(SubscribeType type, String receiverId, String targetId, String extraMessage) {
+        if (type == SubscribeType.BookUpdated) {
+            BookVO bookVO = bookService.findByIdIntegrally(targetId);
+            Notification notification = new Notification();
+            notification.setReceiverId(receiverId);
+            notification.setType(NotificationType.Subscribe);
+            String name = Objects.isNull(bookVO.getTitle()) ? bookVO.getMetadata().getName() : bookVO.getTitle();
+            notification.setMessage(String.format("%s 图书更新了：%s", name, extraMessage));
+            return notification;
+        }
+        return null;
+    }
+
+    @Override
     public Notification generateCancelBy(Subscribe subscribe) {
         if (subscribe.getType() == SubscribeType.BookUpdated) {
             BookVO bookVO = bookService.findByIdIntegrally(subscribe.getBookId());
             Notification notification = new Notification();
             notification.setReceiverId(subscribe.getSubscriberId());
             notification.setType(NotificationType.Subscribe);
-            String name = Objects.isNull(bookVO.getTitle()) ? bookVO.getMetadata().getName() : bookVO.getTitle();
+            String name;
+            if (Objects.isNull(bookVO)) {
+                name = "(已删除数据)";
+            } else {
+                name = Objects.isNull(bookVO.getTitle()) ? bookVO.getMetadata().getName() : bookVO.getTitle();
+            }
             notification.setMessage(String.format("你已取消订阅 %s，后续将不会收到通知！", name));
+            return notification;
         }
         return null;
     }
