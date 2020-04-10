3e16605ec578fdfe0cd40f273a9e3d876a207158
codimiracle <codimiracle@outlook.com>
2020-04-11 03:57:01 +0800

    change to detect user behavior by backend

--- src/main/java/com/codimiracle/application/platform/huidu/web/api/user/ApiUserBuryingPointController.java
+++ src/main/java/com/codimiracle/application/platform/huidu/web/api/user/ApiUserBuryingPointController.java
@@ -1,13 +1,11 @@
 package com.codimiracle.application.platform.huidu.web.api.user;
 
-import com.codimiracle.application.platform.huidu.contract.ApiResponse;
-import com.codimiracle.application.platform.huidu.entity.dto.ReadingHistoryDTO;
-import com.codimiracle.application.platform.huidu.entity.po.User;
 import com.codimiracle.application.platform.huidu.service.UserFigureService;
 import com.codimiracle.application.platform.huidu.web.api.base.BookController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -26,19 +24,4 @@ public class ApiUserBuryingPointController {
 
     @Resource
     private UserFigureService figureService;
-
-    @PostMapping("/history")
-    public ApiResponse history(@AuthenticationPrincipal User user, ReadingHistoryDTO readingHistoryDTO) {
-        return apiHistoryController.reading(user, readingHistoryDTO);
-    }
-
-    @GetMapping("/join-book-shelf")
-    public void joinBookShelf(@AuthenticationPrincipal User user, String bookId) {
-        apiBookShelfController.join(user, bookId);
-    }
-
-    @GetMapping("/visit-book-details")
-    public void visitBookDetails(String bookId) {
-
-    }
 }
